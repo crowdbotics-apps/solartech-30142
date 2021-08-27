d16a67957f15f47aee057781febd9806c790c633
Crowdbotics <team@crowdbotics.com>
2021-08-27 02:35:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "solartech_30142Identifier",
+  url: "https://solartech_30142Identifier.botics.co",
+  api: "https://solartech_30142Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
